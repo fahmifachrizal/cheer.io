a5ddd8b2a89beed1b0cd19b55fbaca3005e01910
Fahmi Fachrizal <34985368+fahmifachrizal@users.noreply.github.com>
2022-11-09 12:44:09 +0700

    update status

---
+++ scripts/script-status.js
@@ -0,0 +1,40 @@
+
+/*
+< !--Single Timeline Content-- >
+<div class="single-timeline-area">
+    <div class="timeline-date wow fadeInLeft" data-wow-delay="0.1s"
+        style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInLeft;">
+    </div>
+    <div class="row">
+        <div class="col-12 col-md-6 col-lg-4">
+            <div class="single-timeline-content d-flex wow fadeInLeft emotion anxiety"                  > pilihan emotion (lowercase)
+                data-wow-delay="0.3s"
+                style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+                <div class="timeline-text">
+                    <h6>Anxiety</h6>                                                                    > pilihan time
+                    <p><small>10.01</small></p>                                                         > pilihan emotion
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+Needs:
+1. time
+2. pilihan emotion
+3. 
+
+{
+    id:
+    username:
+    password:  
+    bio : {age, sex}
+    history: [
+        {emotion_id, time, emotion},
+        {emotion_id, time, emotion},
+        {emotion_id, time, emotion}
+    ]
+}
+
+*/
