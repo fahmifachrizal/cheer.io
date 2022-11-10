e872257427167045000dfa379020bfc0133577b6
attar36 <attarbaqo941@gmail.com>
2022-11-10 20:19:34 +0700

    attar36

---
+++ scripts/script-Article.js
@@ -0,0 +1,27 @@
+/*
+Understanding Mental Health
+Prioritizing mental health applies to each and every one of us. If you’re looking for tips to modify your thinking, better understand mental health conditions, where to get help, or find additional resources – you’ve come to the right place.
+
+
+
+Mental health is a broad topic with many facets. It includes our emotional, psychological, and social well-being and affects how we feel, think, and act. If you struggle with mental health symptoms, you are not alone. While it may seem brave to go it alone, it actually takes strength to reach out for help.
+
+1.Sadness
+Sadness is an emotional pain that usually has a clear cause, such as losing a loved one, a failure at something important, or an unsuccessful goal.
+Feeling sad is normal. Despite our constant efforts to make life perfect and pain-free, sadness will inevitably come, whether it’s from that promotion we didn’t get or a heated argument with a loved one. Everyone has a bad day sometimes, and we all feel sadness at some point in our lives.
+Sadness is also a healthy emotion. Although it might not be the most comfortable emotion, as we usually tend to do our best to avoid it, sadness is valid. It teaches you to appreciate life, gives you insight into the inner worlds of others, and allows you to process life changes.
+2.Anxiety 
+disorders are one of the most common mental health illnesses in the United States. It’s normal to feel anxious before a test, on the first day of work or in a new social situation. But when feeling anxious is relentless and impacts your daily life and decisions you make, it may be time to speak with a mental health professional.
+3. Fear
+Fear is an intensely unpleasant emotion in response to perceiving or recognizing a danger or threat. Fear causes physiological changes that may produce behavioral reactions such as mounting an aggressive response or fleeing the threat. Fear in human beings may occur in response to a certain stimulus occurring in the present, or in anticipation or expectation of a future threat perceived as a risk to oneself. The fear response arises from the perception of danger leading to confrontation with or escape from/avoiding the threat (also known as the fight-or-flight response), which in extreme cases of fear (horror and terror) can be a freeze response or paralysis.
+In humans and other animals, fear is modulated by the process of cognition and learning. Thus, fear is judged as rational or appropriate and irrational or inappropriate. An irrational fear is called a phobia.
+Fear is sometimes incorrectly considered the opposite of courage. Because courage is a willingness to face adversity, fear is an example of a condition that makes the exercise of courage possible.
+4. Anger
+Anger is an emotion characterized by antagonism toward someone or something you feel has deliberately done you wrong.
+Anger can be a good thing. It can give you a way to express negative feelings, for example, or motivate you to find solutions to problems.
+But excessive anger can cause problems. Increased blood pressure and other physical changes associated with anger make it difficult to think straight and harm your physical and mental health.
+
+
+Strengthening Your Mental Fitness
+Life is not without challenges, and we all feel stress and anxiety at times. Learning how to effectively manage stress and implementing practices of self-care may improve mental health and well-being.
+*/
