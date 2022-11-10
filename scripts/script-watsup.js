ec703e6e3777b1bc8b552713c87426c888e99e78
Fahmi Fachrizal <34985368+fahmifachrizal@users.noreply.github.com>
2022-11-10 18:49:07 +0700

    fix insyaallah jadi

---
+++ scripts/script-watsup.js
@@ -0,0 +1,19 @@
+const users = JSON.parse(localStorage.list_users)
+let username = sessionStorage.username
+let user = {}
+
+for (let key in users) {
+    let val = users[key]
+    if (val.username === username) {
+        user = val
+    }
+}
+profilename = user.bio.fullname
+
+
+
+// List of event listener
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById('profile-name').innerText += ` ${profilename}`
+}
+)
